3ce7e5569087ec507477114450b7c4b6b18346d1
José Manuel Déniz Suárez <JoseDeniz@users.noreply.github.com>
2017-04-12 10:48:15 +0100

    Add a new employee

--- src/main/java/org/josedeniz/payroll/dataloaders/DatabaseLoader.java
+++ src/main/java/org/josedeniz/payroll/dataloaders/DatabaseLoader.java
@@ -19,5 +19,6 @@ public class DatabaseLoader implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.repository.save(new Employee("Jose", "Deniz", "apprentice"));
     }
 }
