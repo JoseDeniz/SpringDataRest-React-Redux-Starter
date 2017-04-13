676a7113ff63bfc82eab0acda48e4e3e13f3b2e7
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2017-04-13 18:00:48 +0100

    Add fallback route to serve the front

--- src/main/java/org/josedeniz/payroll/Application.java
+++ src/main/java/org/josedeniz/payroll/Application.java
@@ -2,9 +2,18 @@ package org.josedeniz.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public EmbeddedServletContainerCustomizer notFoundCustomizer() {
+        return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/org/josedeniz/payroll/controller/HomeController.java
+++ src/main/java/org/josedeniz/payroll/controller/HomeController.java
@@ -1,12 +1,12 @@
 package org.josedeniz.payroll.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/", produces = "text/html")
     public String index() {
         return "index";
     }
